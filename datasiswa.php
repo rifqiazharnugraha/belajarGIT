89306baafa36220cf6d718f35c85ad63a9b9aaa1
Rifqi Azhar Nugraha <rifqi.an@gmail.com>
2023-08-21 08:34:48 +0700

    Create datasiswa.php

---
+++ datasiswa.php
@@ -0,0 +1,10 @@
+Disini adalah Halaman untuk Menampilkan Data Siswa dengan menggunakan Table
+Table Data Siswa diantaranya:
+id
+Nama Lengkap
+Jenis Kelamin
+Kelas
+Jurusan
+Action:
+  - Edit
+  - Delete
